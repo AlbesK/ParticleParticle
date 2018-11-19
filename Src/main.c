e70e89fc6af62a4f8a740525d10922b9c8318ce1
AlbesK <44774494+AlbesK@users.noreply.github.com>
2018-11-19 10:19:48 +0000

    Updating main.c
    Updated main.c with a Functions.h header and focus more on sub-functions being called to main.c rather than just directly copying the files with #include inside the blocks.

--- Src/main.c
+++ Src/main.c
@@ -2,19 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-
-////Number of Particles
-//#define  N_PARTICLES 3 
-
-////Number of Dimensions the particles reside in
-//#define  N_DIMENSIONS 2 
+#include "Functions.h"
 
 int main()
 {
- 
+  ///Number of Particles & Number of Dimensions the particles reside in
   int N_PARTICLES;
-  int N_DIMENSIONS = 2;
+  int N_DIMENSIONS = 3;
   int readin;
+  clock_t start, end;
+
+  double (*A)[N_DIMENSIONS] = malloc(sizeof(double[N_PARTICLES][N_DIMENSIONS])); //Dynamically allocate memory for Array-
+  double *Mass = malloc(sizeof(double) * N_PARTICLES); //- for memory
+  double *V = malloc(sizeof(double) * N_PARTICLES); //- for Potentials
+  double (*F)[N_DIMENSIONS] = malloc(sizeof(double[N_PARTICLES][N_DIMENSIONS])); //- for Forces
 
   printf("How many particles?\n");
   readin = scanf("%i", &N_PARTICLES);
@@ -22,33 +23,34 @@ int main()
     printf("Particle number read failed\n");
     exit(-1);
   }
+
+  printf("Calculating\n");
   if (N_PARTICLES < 2) {
     printf("Insufficent number of particles %i\n", N_PARTICLES);
     exit(-1);
   }
+  printf("-----\n");
   
 
-    srand(clock()); // seed random number generator
-
-    clock_t start, end;
+  initialiser(N_PARTICLES, N_DIMENSIONS, A, Mass);
+  printf("-----\n");
+   
+  start = clock(); //start timer
     
-    #include "initl1.c"  //Initialiser
-
-    start = clock(); //start timer
+  ppmodel(N_PARTICLES, N_DIMENSIONS, V, F, A, Mass); //Model
     
-    #include "PPmodel.c"  //Model
+  end = clock(); //end timer
     
-    end = clock(); //end timer
+  double duration = (double)(end-start)/CLOCKS_PER_SEC;
+  printf("Time elapsed is: %f (s)\n", duration);
     
-    double duration = (double)(end-start)/CLOCKS_PER_SEC;
-    printf("Time elapsed is: %f (s)\n", duration);
+  //#include "printer.c" //Print file.c
     
-    #include "printer.c" //Print file.c
-
-    free(A); //Free memory
-    free(mass);
-    free(V);
-    free(F);
-
-    return 0;
+  free(A); //Free memory
+  free(Mass);
+  printf("Released the memory succesfuly\n");
+  //free(V);
+  //free(F);
+    
+  return 0;
 }
