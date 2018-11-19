fe63193550b3cf9b2e904f0f99effd474ee6916b
AlbesK <44774494+AlbesK@users.noreply.github.com>
2018-11-19 11:55:58 +0000

    Just changed an asterisk pointer symbol

--- Src/Functions.h
+++ Src/Functions.h
@@ -2,6 +2,6 @@
 #define _FUNCTIONS
 
 void initialiser(int i, int j, double (*A)[j], double* Mass);
-void ppmodel(int i, int j, double *V, double (*F)[j], double (*A)[j], double* Mass);
+void ppmodel(int i, int j, double* V, double (*F)[j], double (*A)[j], double* Mass);
 
 #endif
