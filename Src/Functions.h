293f2fe0c88dcf69eb10c3cb3c3e0f1e0b727a12
AlbesK <44774494+AlbesK@users.noreply.github.com>
2018-11-19 10:22:02 +0000

    Adding a header with functions
    Including a header "Functions.h" to be used along main and each sub-function.c file needed.

---
+++ Src/Functions.h
@@ -0,0 +1,7 @@
+#ifndef _FUNCTIONS
+#define _FUNCTIONS
+
+void initialiser(int i, int j, double (*A)[j], double* Mass);
+void ppmodel(int i, int j, double *V, double (*F)[j], double (*A)[j], double* Mass);
+
+#endif
