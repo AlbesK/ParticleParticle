b3791e5ac3f6a41725f233a91bd8089965b2c4e5
AlbesK <44774494+AlbesK@users.noreply.github.com>
2018-11-25 22:23:36 +0000

    Update initialiser.c with charges.

--- Src/initialiser.c
+++ Src/initialiser.c
@@ -8,22 +8,24 @@ later including charge and masses*/
 #include "Functions.h"
 
 
-void initialiser(int particles, int dimensions, double (*A)[dimensions], double* Mass ){ // i for particles, j for dimensions
+void initialiser(int particles, int dimensions, double (*A)[dimensions], double* Mass, double* Charge ){ // i for particles, j for dimensions
 
 /*Loop through the array and initialise the position with random values between */ 
 
 srand(time(NULL)); //Seed time for random value generation 
 
-for (int i=0; i<particles; i++){
-  for(int j=0; j<dimensions; j++){
-      *(*A+j) = 10 * ( (double) rand() / (double) RAND_MAX );
-      printf("A[%i][%i] = %f\n", i, j, *(*A+j) );
+for (int i=0; i < particles; i++){
+  for(int j=0; j < dimensions; j++){
+      A[i][j] = 10 * ( (double) rand() / (double) RAND_MAX );
+      printf("A[%i][%i] = %f\n", i, j, A[i][j] );
   }
 }
 
-for (int i=0; i<particles; i++){
-    *(Mass+i) = 5 * ((double) rand() / (double) RAND_MAX );
-    printf("Masses are %f \n", *(Mass+i));
+for (int i = 0; i < particles; i++){
+    Mass[i] = 5 * ((double) rand() / (double) RAND_MAX );
+    Charge[i] = 10 * ((double) rand() / (double) RAND_MAX ) - 5;
+    printf("Mass[%i]: %f \n", i, Mass[i]);
+    printf("Charge[%i]: %f \n", i,  Charge[i]);
 }
 
 }
