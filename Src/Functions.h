e68e5fdf9c268ac2904756655ad9c4c451299c8c
AlbesK <44774494+AlbesK@users.noreply.github.com>
2018-11-25 22:21:30 +0000

    Update Functions.h
    Adding Charges, leaving mass for now.

--- Src/Functions.h
+++ Src/Functions.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTIONS
 #define _FUNCTIONS
 
-void initialiser(int i, int j, double (*A)[j], double* Mass);
-void ppmodel(int i, int j, double* V, double (*F)[j], double (*A)[j], double* Mass);
+void initialiser(int i, int j, double (*A)[j], double* Mass, double* Charge);
+void ppmodel(int i, int j, double* V, double (*F)[j], double (*A)[j], double* Mass, double* Charge);
 
 #endif
