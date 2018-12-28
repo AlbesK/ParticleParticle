0e6401ece5b4fc603e0b76492c1b2a7588e75dd0
Albes K <koxhaj19@hotmail.com>
2018-12-28 23:38:47 +0000

    Started adding objects and deleted the references for the previous test

--- Src_C++/main.cpp
+++ Src_C++/main.cpp
@@ -3,12 +3,59 @@
 #include <ctime>
 #include <cmath>
 #include <vector>
+#include <algorithm>
+#include <iterator>
 //#include "Functions.h"
 //ios::sync_with_stdio(false);
 
 int main()
 {
   // Particle Number and Dimensions
+
+  //Object approach
+  class Body {
+    private:
+
+    std::vector<double> position{2}; 
+    double mass;
+    double charge;
+
+    public:
+
+    Body(double x = 1.0, double y = 1.0, double z = 1.0, double m = 1.0, double c = -1.0)
+    { //Default Constructor
+      this->position[0] = x;
+      this->position[1] = y;
+      this->position[2] = z;
+      this->mass = m;
+      this->charge = c;
+    }
+
+    //~Body();//Destructor
+
+    double getPosition(){
+      for (int i=0; i< 2; ++i){
+        std::cout << position[i] << " ";
+      }
+    }
+
+    double getMass(){
+      return this->mass;
+    }
+
+    double getCharge(){
+      return this->charge;
+    }
+
+    int getSize(){
+      return position.size();
+    }
+
+
+
+  };
+
+  Body b1;
 
   int N_PARTICLES = 4; int N_DIMENSIONS = 3; int seed=1; 
   
@@ -21,28 +68,21 @@ int main()
   Mass = new std::vector<double>(N_PARTICLES);
   Charge = new std::vector<double>(N_PARTICLES);
 
-  //Creating references
-  std::vector< std::vector<double> > &Matrix_ref = *Matrix;
-  std::vector<double> &Mass_ref = *Mass;
-  std::vector<double> &Charge_ref = *Charge;
-
   srand(seed); //Seed time for random value generation 
 
+  //Initialising the matrices
 
   for (int i=0; i < N_PARTICLES; i++){
     for(int j=0; j < N_DIMENSIONS; j++){
-      //Matrix_ref[i][j] = (10 * ( (double) rand() / (double) RAND_MAX ));
-      //std::cout << Matrix_ref[i][j] << std::endl;
+
       (*Matrix)[i][j] = (10 * ( (double) rand() / (double) RAND_MAX ));
       std::cout << (*Matrix)[i][j] << std::endl;
+    
     } 
   }
 
   for (int i = 0; i < N_PARTICLES; i++){
-    //Mass_ref[i] = (5 * ((double) rand() / (double) RAND_MAX ));
-    //Charge_ref[i] = (10 * ((double) rand() / (double) RAND_MAX ) - 5);
-    //std::cout << "Mass is" << i << Mass_ref[i] << std::endl;
-    //std::cout << "Charge is" << i << Charge_ref[i] << std::endl;
+    
     (*Mass)[i] = (5 * ((double) rand() / (double) RAND_MAX ));
     (*Charge)[i] = (10 * ((double) rand() / (double) RAND_MAX )-5);
     std::cout << "Mass is " << i << (*Mass)[i] << std::endl;
@@ -50,9 +90,18 @@ int main()
     
 
   }
+  //Checking Purposes
 
   std::cout << "Check" << std::endl;
-  std::cout << "Now sizes are: For Matrix:" << Matrix->size() << " Then for Mass:" << Mass->size() << " Finally for Charge:" << Charge->size() << std::endl;
+  std::cout << "Now sizes are: For Matrix:" << Matrix->size() << " Then for Mass:" << Mass->size() << " Finally for Charge:" 
+  << Charge->size() << std::endl;
+  std::cout << " Body default parameters are: " << b1.getPosition() << " Mass is: "
+  << b1.getMass() << "Charge is: " << b1.getCharge() << std::endl;
+
+  std::cout << b1.getSize() << std::endl;
+
+
+  //Free Memory
   delete Matrix; delete Mass; delete Charge;
 
 }
