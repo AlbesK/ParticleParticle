cc8e8ac46fbd4b5f7e58f08323c26cd70b0c3315
Albes K <koxhaj19@hotmail.com>
2018-12-29 12:02:35 +0000

    Making some more tweeks on the new class Body with also some more cout statements to check whats going on

--- Src_C++/main.cpp
+++ Src_C++/main.cpp
@@ -12,31 +12,29 @@ int main()
 {
   // Particle Number and Dimensions
 
-  //Object approach
+  int N_PARTICLES = 4; int N_DIMENSIONS = 3; int seed=1; 
+
+   //Object approach
   class Body {
     private:
 
-    std::vector<double> position{2}; 
-    double mass;
-    double charge;
+    std::vector<double> position; //Position
+    double mass;   //Mass
+    double charge;  //Charge
 
     public:
 
-    Body(double x = 1.0, double y = 1.0, double z = 1.0, double m = 1.0, double c = -1.0)
+    Body(double rx = 1.0, double ry = 2.0, double rz = 3.0, double m = 1.0, double c = -1.0)
     { //Default Constructor
-      this->position[0] = x;
-      this->position[1] = y;
-      this->position[2] = z;
+      this->position = {rx,ry,rz};
       this->mass = m;
       this->charge = c;
     }
 
     //~Body();//Destructor
 
-    double getPosition(){
-      for (int i=0; i< 2; ++i){
-        std::cout << position[i] << " ";
-      }
+    std::vector<double> getPosition(){
+      return this->position;
     }
 
     double getMass(){
@@ -47,17 +45,9 @@ int main()
       return this->charge;
     }
 
-    int getSize(){
-      return position.size();
-    }
-
-
-
   };
 
   Body b1;
-
-  int N_PARTICLES = 4; int N_DIMENSIONS = 3; int seed=1; 
   
   //Pointers and their initialization
   std::vector< std::vector<double> > *Matrix;
@@ -91,14 +81,13 @@ int main()
 
   }
   //Checking Purposes
-
+  std::vector<double> vec_pos = b1.getPosition();
   std::cout << "Check" << std::endl;
   std::cout << "Now sizes are: For Matrix:" << Matrix->size() << " Then for Mass:" << Mass->size() << " Finally for Charge:" 
   << Charge->size() << std::endl;
-  std::cout << " Body default parameters are: " << b1.getPosition() << " Mass is: "
-  << b1.getMass() << "Charge is: " << b1.getCharge() << std::endl;
+  std::cout << "Body default parameters are: " << vec_pos[0] << vec_pos[1] << vec_pos[2] << " Mass is: " << b1.getMass() << "Charge is: " << b1.getCharge() << std::endl;
 
-  std::cout << b1.getSize() << std::endl;
+  
 
 
   //Free Memory
